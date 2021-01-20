825a5c4beb7dbfa3a294c06aa77483de4cbe3b21
jdquerales <jquerales72@gmail.com>
2021-01-20 21:28:34 +0000

    Add signup form, mongoDB document created satisfactory

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, session, redirect
+from functools import wraps
 from flask_pymongo import PyMongo
 from passlib.hash import pbkdf2_sha256
 import uuid
@@ -17,6 +18,56 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 db = mongo.db
+
+
+# decorators
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect('/')
+    return wrap
+
+
+class User:
+    def start_session(self, user):
+        del user['password']
+        session['logged_in'] = True
+        session['user'] = user
+        return jsonify(user), 200
+
+    def signup(self):
+        print(request.form)
+
+        user = {
+            "_id": uuid.uuid4().hex,
+            "name": request.form.get('name'),
+            "email": request.form.get('email'),
+            "password": request.form.get('password')
+        }
+
+# Encrypt the password
+        user['password'] = pbkdf2_sha256.encrypt(user['password'])
+        # Check for existing email address
+        if db.users.find_one({"email": user['email']}):
+            return jsonify({"error": "Email address already in use"}), 400
+        if db.users.insert_one(user):
+            return self.start_session(user)
+        return jsonify({"error": "Signup failed"}), 400
+
+    def signout(self):
+        session.clear()
+        return redirect('/')
+
+    def login(self):
+        user = db.users.find_one({"email": request.form.get('email')})
+        if user and pbkdf2_sha256.verify(
+                                        request.form.get('password'),
+                                        user['password']):
+            return self.start_session(user)
+        return jsonify({"error": "Invalid login credentials"}), 401
 
 
 @app.route('/')
@@ -42,6 +93,14 @@ def signin():
 @app.route('/signout')
 def signout():
     return render_template('signout.html')
+
+
+@app.route('/signup', methods=['POST','GET'])
+def signup():
+    if request.method == 'POST':
+        return User().signup()
+
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,49 @@
 
 // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+$('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
-  });
+});
+
+$("form[name=signup_form").submit(function (e) {
+
+    var $form = $(this);
+    var $error = $form.find(".error")
+    var data = $form.serialize();
+
+    $.ajax({
+        url: "/signup",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function (resp) {
+            window.location.href = "/dashboard"
+        },
+        error: function (resp) {
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+
+    e.preventDefault();
+});
+
+$("form[name=login_form").submit(function (e) {
+
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url: "/signin",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function (resp) {
+            window.location.href = "/dashboard/";
+        },
+        error: function (resp) {
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+
+    e.preventDefault();
+});
