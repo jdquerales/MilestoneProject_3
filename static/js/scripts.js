12a90c1c98c936be19f428505a43b4daf7b4b4b2
jdquerales <jquerales72@gmail.com>
2021-02-04 21:50:12 +0000

    Add Jquery function to highlight current page on navbar

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,15 +2,24 @@
 // Closes responsive menu when a scroll trigger link is clicked
 $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
-    $('#mainNav').css("background-color","transparent");
+    $('#mainNav').css("background-color", "transparent");
 });
 
 
 // Add JQuery event for mainNav background color:
-$('.navbar-toggler').click(function() {
-    $("#mainNav").css("background-color","gray");
+$('.navbar-toggler').click(function () {
+    $("#mainNav").css("background-color", "gray");
 });
 
+
+// This function highligth current page
+$(function () {
+    $('li a').each(function () {
+        if ($(this).prop('href') == window.location.href) {
+            $(this).addClass('active'); $(this).parents('li').addClass('active');
+        }
+    });
+});
 
 
 $("form[name=signup_form]").submit(function (e) {
