1628a7449583f801cc4b17210a154f602417ed6f
jdquerales <jquerales72@gmail.com>
2021-02-07 16:24:01 +0000

    Change and add new locations on Google Maps

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,28 +1,32 @@
-	function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
-                center: {
-                    lat: 46.619261,
-                    lng: -33.134766
-                }
-            });
-
-            var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-            var locations = [
-                {lat: 40.785091, lng: -73.968285},
-                {lat: 41.084045, lng: -73.874245},
-                {lat: 40.754932, lng: -73.984016}
-            ];
-
-            var markers = locations.map(function(location, i) {
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
-                });
-            });
-
-            var markerCluster = new  MarkerClusterer(map, markers, {
-    imagePath:"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"});
-
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 2,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
         }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: -34.6083, lng: -58.3712 },
+        { lat: 53.33306, lng: -6.24889 },
+        { lat: 52.531677, lng: 13.381777 },
+        { lat: 40.4168, lng: -3.703790 },
+        { lat: -23.5499978, lng: -46.625290 },
+        { lat: 19.432608, lng: -99.133209 }
+    ];
+
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
+    });
+
+}
